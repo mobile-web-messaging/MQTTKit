a51a83bbe2c4fcc66a44f5316a03c49148165466
Jeff Mesnil <jmesnil@gmail.com>
2013-10-22 17:01:06 +0200

    Initial Commit

---
+++ MQTTKit/MQTTKit.h
@@ -0,0 +1,13 @@
+//
+//  MQTTKit.h
+//  MQTTKit
+//
+//  Created by Jeff Mesnil on 22/10/2013.
+//  Copyright (c) 2013 Jeff Mesnil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MQTTKit : NSObject
+
+@end
